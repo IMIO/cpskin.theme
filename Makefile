496ddaffaaf4ccecf0c348d725d6d8e37001ce1d
Benoit Suttor <bsuttor@imio.be>
2018-08-28 15:56:04 +0200

    Fixing tests

--- Makefile
+++ Makefile
@@ -1,15 +1,12 @@
 #!/usr/bin/make
 .PHONY: buildout cleanall test instance
 
-bootstrap.py:
-	wget http://downloads.buildout.org/2/bootstrap.py
-
-bin/python:
-	virtualenv-2.7 .
+bin/pip:
+	if [ -f /usr/bin/virtualenv-2.7 ] ; then virtualenv-2.7 .;else virtualenv -p python2.7 .;fi
 	touch $@
 
-bin/buildout: bootstrap.py buildout.cfg bin/python
-	./bin/python bootstrap.py
+bin/buildout: bin/pip
+	./bin/pip install -r requirements.txt
 	touch $@
 
 buildout: bin/buildout
@@ -21,6 +18,9 @@ test: buildout
 instance: buildout
 	./bin/instance fg
 
-
-cleanall: 
+cleanall:
 	rm -rf bin develop-eggs downloads include lib parts .installed.cfg .mr.developer.cfg bootstrap.py parts/omelette
+
+docker-test:
+	docker pull docker-staging.imio.be/iasmartweb/test:$(shell id -u)
+	docker run -u $(shell id -u):$(shell id -g) --rm -v "$(shell pwd):/src" -w /src docker-staging.imio.be/iasmartweb/test:$(shell id -u) bash -c 'virtualenv -p python2.7 . && bin/pip install -r requirements.txt && bin/buildout && /etc/init.d/xvfb start && bin/test --all'
--- cpskin/theme/testing.py
+++ cpskin/theme/testing.py
@@ -1,15 +1,29 @@
-from plone.app.testing import PloneWithPackageLayer
+# -*- coding: utf-8 -*-
 from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneWithPackageLayer
+from plone.testing import z2
 
 import cpskin.theme
 
 
-CPSKIN_THEME_FIXTURE = PloneWithPackageLayer(
-    name="CPSKIN_THEME_FIXTURE",
-    zcml_filename="testing.zcml",
+class CPSkinThemePloneWithPackageLayer(PloneWithPackageLayer):
+    def setUpZope(self, app, configurationContext):
+        super(CPSkinThemePloneWithPackageLayer, self).setUpZope(
+            app,
+            configurationContext
+        )
+        z2.installProduct(app, 'Products.DateRecurringIndex')
+
+    def tearDownZope(self, app):
+        z2.uninstallProduct(app, 'Products.DateRecurringIndex')
+
+
+CPSKIN_THEME_FIXTURE = CPSkinThemePloneWithPackageLayer(
+    name='CPSKIN_THEME_FIXTURE',
+    zcml_filename='testing.zcml',
     zcml_package=cpskin.theme,
-    gs_profile_id="cpskin.theme:testing")
+    gs_profile_id='cpskin.theme:testing')
 
 CPSKIN_THEME_INTEGRATION_TESTING = IntegrationTesting(
     bases=(CPSKIN_THEME_FIXTURE,),
-    name="CPSkinTheme:Integration")
+    name='CPSkinTheme:Integration')
